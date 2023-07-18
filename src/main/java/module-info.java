c9c18346441a508787b5bdac3dd8be1e2972b5c9
USER <mo.javad.bijani@gmail.com>
2023-07-18 11:59:23 +0330

    salam

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,8 @@ module com.example.phase3 {
 
     opens com.example.phase3 to javafx.fxml;
     exports com.example.phase3;
+
+    opens controllers to javafx.fxml;
+    opens views to javafx.fxml;
+    opens models to com.google.gson;
 }
